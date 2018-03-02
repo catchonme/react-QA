8b038eaed404851348c23063ed2b5da5f58b7a54
catchme <izengdp@163.com>
2018-03-02 16:39:36 +0800

    完成问题页面问题的切换

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import {
   Link,
   Route
 } from 'react-router-dom'
-import QuestionList from '../containers/QuestionList'
+import Question from '../containers/Question'
 
 const Home = () => (
   <Router>
     <div className="home_body">
       <div className='top_tips'><div className="num_tip">第一周</div> </div>
       <div className='home_logo item_container_style'></div>
-      <Link to={{pathname:"/question-list/1",state:{question:2}}}><div className="start button_style"></div></Link>
-      <Route path="/question-list/:id" ques_id={2} component={QuestionList}/>
+      <Link to="/question"><div className="start button_style"></div></Link>
+      <Route path="/question" component={Question}/>
     </div>
   </Router>
 )
--- src/containers/Question.js
+++ src/containers/Question.js
@@ -1,56 +1,107 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from "react-redux";
+import {changeActive, chooseAnswer} from "../actions";
 import {
   BrowserRouter as Router,
   Link,
   Route
 } from 'react-router-dom'
 import Result from '../components/Result'
-import { changeActive } from "../actions";
-import { connect } from 'react-redux'
 
-let Question = ({question, active, match, dispatch}) => {
+class SingleQuestion extends Component {
 
-  const chooseType = (index) => {
-    switch(index) {
-      case 0 : return 'A';break;
-      case 1 : return 'B';break;
-      case 2 : return 'C';break;
-      case 3 : return 'D';break;
-      default : return '';
+  static propTypes = {
+    questions: PropTypes.array.isRequired,
+    changeQuestionActive: PropTypes.func.isRequired,
+  }
+
+  state = {
+    question_index: 0,
+    question:{}
+  }
+
+  incrementIndex = (question_index) => {
+    const { questions } = this.props
+    this.setState({question_index: question_index + 1, question:questions[question_index + 1]})
+  }
+
+  renderButton({question_index, length}) {
+    if (question_index < (length-1)) {
+      return (
+        <div className='next_item button_style' onClick={() => this.incrementIndex(question_index)}></div>
+      )
+    } else {
+      return (
+        <div>
+          <Link to='/result'><div className='submit_item button_style'></div></Link>
+          <Route path='/result' component={Result}/>
+        </div>
+      )
     }
   }
 
-  const currentQuestion = {}
-  const getCurrentQuestion = () => {
-    console.log(match);
-    return match.params.id;
+  componentWillMount() {
+    const { questions } = this.props;
+    const {question_index} = this.state
+    const length = questions.length;
+    const temp_question_index = question_index > (length-1) ? (length-1) : question_index;
+    const question = questions[temp_question_index];
+    this.setState({question_index:temp_question_index, question:question})
   }
 
-  return (
-    <Router>
-      <div className="question_body">
-        <div className='top_tips'><div className="num_tip">题目{match.params.id}</div> </div>
-        <div className='item_back item_container_style'></div>
-        <div className='item_list_container'>
-          <div className="item_title">{question.topic_name}{getCurrentQuestion()}</div>
-          <ul>
-            {
-              question.topic_answer.map((option, index) => {
-                return <li className="item_list" onClick={() => dispatch(changeActive(index))} key={index}>
+  render() {
+    const { active, questions, changeQuestionActive } = this.props
+    const length = questions.length;
+    const { question, question_index } = this.state
+    const chooseType = (index) => {
+      switch(index) {
+        case 0 : return 'A';break;
+        case 1 : return 'B';break;
+        case 2 : return 'C';break;
+        case 3 : return 'D';break;
+        default : return '';
+      }
+    }
+
+    return (
+      <Router>
+        <div className="question_body">
+          <div className='top_tips'><div className="num_tip">题目</div> </div>
+          <div className='item_back item_container_style'></div>
+          <div className='item_list_container'>
+            <div className="item_title">{question.topic_name}</div>
+            <ul>
+              {
+                question.topic_answer.map((option, index) => {
+                  return <li className="item_list" onClick={() => changeQuestionActive(index)} key={index}>
                     <span className={active === index ? 'has_choosed option_style' : 'option_style'}>{chooseType(index)}</span>
                     <span className="option_detail">{option.answer_name}</span>
-                </li>
-              })
-            }
-          </ul>
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+          {this.renderButton({question_index, length})}
         </div>
-        <Link to="/result"><div className="submit_item button_style"></div></Link>
-        <Route path="/result" component={Result}/>
-      </div>
-    </Router>
-  )
+      </Router>
+    )
+  }
 }
 
-Question = connect()(Question);
+const mapStateToProps = (state) => ({
+  active: state.questions.active,
+  questions: state.questions.itemDetail
+})
+
+const mapDispatchToProps = {
+  onClick: chooseAnswer,
+  changeQuestionActive : changeActive
+}
+
+const Question = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SingleQuestion)
 
 export default Question
