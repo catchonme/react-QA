fc07c21c14c63f122b33b1ad16def95dbaa0a2ac
catchme <izengdp@163.com>
2018-03-03 10:49:05 +0800

    完成结果页面的点击分享

--- src/containers/Result.js
+++ src/containers/Result.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import classnames from 'classnames'
 
 class ShowResult extends Component {
   static propTypes = {
@@ -11,11 +12,13 @@ class ShowResult extends Component {
 
   state = {
     score:0,
-    score_tip:''
+    score_tip:'',
+    show_cover:false
   }
 
-  showCover = (e) => {
-    e.stopPropagation();
+  showCover = () => {
+    const { show_cover } = this.state;
+    this.setState({show_cover:!show_cover})
   }
 
   componentWillMount() {
@@ -29,34 +32,33 @@ class ShowResult extends Component {
       case 10 :
       case 20 :  score_tip = scoreTips[0]; break;
       case 30 :
-      case 40 :  score_tip = scoreTips[1];break;
+      case 40 :  score_tip = scoreTips[1]; break;
       case 50 :
-      case 60 :  score_tip = scoreTips[2];break;
+      case 60 :  score_tip = scoreTips[2]; break;
       case 70 :
-      case 80 :  score_tip = scoreTips[3];break;
+      case 80 :  score_tip = scoreTips[3]; break;
       case 90 :
-      case 100:  score_tip = scoreTips[4];break;
+      case 100:  score_tip = scoreTips[4]; break;
     }
     this.setState({score:score, score_tip:score_tip})
   }
 
   render() {
-    const { score, score_tip } = this.state
+    const { score, score_tip, show_cover } = this.state
     return (
       <div className="result_body">
         <div className="your_scores_container">
           <header className="your_scores"><span className="score_num">{score}</span><span className="fenshu">分！</span></header>
           <div className="result_tip">{score_tip}</div>
         </div>
-        <div className="share_button" onClick={this.showCover}></div>
+        <div className="share_button" onClick={()=>this.showCover()}></div>
         <div className="share_code">
           <header className="share_header">关注葡萄之家，获取答案。</header>
           <img src={require('../images/4-4.png')} alt="qr_code" height="212" width="212" className="code_img"/>
-          {/*<div className="share_img"></div>*/}
         </div>
-        {/*<div className="share_cover" onClick={showCover}>
-        <div className="share_img"></div>
-      </div>*/}
+        <div className={classnames({'share_cover':true,'hide_cover':!show_cover})} onClick={()=>this.showCover()}>
+          <img src={require('../images/5-2.png')} className="share_img"></img>
+        </div>
       </div>
     )
   }
